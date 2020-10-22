23a4ea495bb708d8b0fc8c11f37c5edcd69aaccb
Jordan Guggenheim <gugges@gmail.com>
2020-10-22 12:28:35 -0400

    Make cancelImageDownload internal to simplify the Public API. Add documentation.

--- Sources/Extensions/UIButton+ImageDownloader.swift
+++ Sources/Extensions/UIButton+ImageDownloader.swift
@@ -10,6 +10,7 @@ import UIKit
 
 public extension ObjectWrapper where T: UIButton {
 
+    /// A convenience property that when set will download (or cancel if nil) the url. The getter returns the active imageDownloaderReceipt URL.
     var imageUrl: URL? {
         get {
             imageDownloaderReceipt?.url
@@ -19,12 +20,19 @@ public extension ObjectWrapper where T: UIButton {
         }
     }
     
+    /// A convenience method to set an image based on a URL. This will only set for the .normal state of the UIButton.
+    /// - Parameters:
+    ///   - url: the HTTP URL for the image download. If set to nil, it will cancel any active requests based on the imageDownloaderReceipt. Note: This does not currently support local file system URLs.
+    ///   - imageDownloader: The Image Downloader shared instance. Used for unit testing injection of mocks.
+    ///   - completionHandler: If set to nil, the image will be automatically set on completion. If completionHandler is set, the image will not be automatically set and instead returned via the completionHandler for further modifications (resizing, image filters, custom animation transitions, etc.)
+    /// - Returns: The receipt for the active image download.
+    @discardableResult
     func setImage(url: URL?,
                   imageDownloader: ImageDownloading = ImageDownloader.shared,
-                  completionHandler: ImageDownloader.CompletionHandler? = nil) {
+                  completionHandler: ImageDownloader.CompletionHandler? = nil) -> ImageDownloaderReceipt? {
         guard let url = url else {
             cancelImageDownload(imageDownloader: imageDownloader)
-            return
+            return nil
         }
         
         if imageDownloaderReceipt != nil {
@@ -50,6 +58,8 @@ public extension ObjectWrapper where T: UIButton {
             
             completionHandler(result, downloadReceipt)
         }
+        
+        return imageDownloaderReceipt
     }
     
 }
